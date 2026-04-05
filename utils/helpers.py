45fb7fb94063a0b5e49d4d68efce8de40e6daf39
xoromfollow <xoromfollow@gmail.com>
2026-04-05 00:25:22 -0700

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,7 @@
+# Utility functions for GenLayer Intelligent Contracts
+
+def validate_positive_amount(amount: int) -> bool:
+    """Simple helper to validate amounts"""
+    return amount > 0
+
+# Add more helpers here as you build (LLM prompt templates, data validation, etc.)
